faf769631b862c3bc474d3db42daf7409980708b
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2015-04-16 20:43:20 +0200

    Moved water molecule to it's own class

---
+++ src/main/scala/scala_tryouts/Molecule.scala
@@ -0,0 +1,47 @@
+package scala_tryouts
+
+import scalafx.scene.shape.Sphere
+import scalafx.scene.shape.Cylinder
+import scalafx.scene.Group
+import scalafx.scene.paint.PhongMaterial
+import scalafx.scene.transform.Rotate
+import scalafx.scene.transform.Translate
+import scalafx.scene.paint.Color
+
+class Hydrogen extends Group {
+  val conn_mat = new PhongMaterial {
+    diffuseColor = Color.LightBlue
+    specularColor = Color.White
+  }
+  val hydro_mat = new PhongMaterial {
+    diffuseColor = Color.Red
+    specularColor = Color.White
+  }
+  
+  children = {
+    new Sphere(100) {
+      material = hydro_mat
+    } ::
+    new Cylinder(25, 200) {
+      material = conn_mat
+      transforms = new Translate(200, 0, 0) :: new Rotate(90, Rotate.ZAxis) :: Nil
+    } :: Nil
+  }
+}
+
+class Water extends Group {
+  val water_mat = new PhongMaterial {
+    diffuseColor = Color.Blue
+    specularColor = Color.White
+  }
+  
+  val hydrogen1 = new Hydrogen
+  val hydrogen2 = new Hydrogen
+  val water = new Sphere(200) {
+    material = water_mat
+  }
+  children = water :: hydrogen1 :: hydrogen2 :: Nil
+  
+  hydrogen1.transforms = new Rotate( -45, Rotate.ZAxis) :: new Translate(-500, 0, 0) :: Nil
+  hydrogen2.transforms = new Rotate(-135, Rotate.ZAxis) :: new Translate(-500, 0, 0) :: Nil
+}
--- src/main/scala/scala_tryouts/ScalaUI.scala
+++ src/main/scala/scala_tryouts/ScalaUI.scala
@@ -17,58 +17,20 @@ import scalafx.scene.paint.PhongMaterial
 import scalafx.scene.shape.Cylinder
 import scalafx.scene.shape.Sphere
 import scalafx.scene.transform.Rotate
-import scalafx.stage.Stage
 import scalafx.scene.transform.Translate
+import scalafx.stage.Stage
 
 class ScalaScene extends SubScene(1024, 768, true, SceneAntialiasing.Balanced) {
-  val hydro_mat = new PhongMaterial {
-    diffuseColor = Color.Red
-    specularColor = Color.White
-  }
-  val conn_mat = new PhongMaterial {
-    diffuseColor = Color.LightBlue
-    specularColor = Color.White
-  }
-  val water_mat = new PhongMaterial {
-    diffuseColor = Color.Blue
-    specularColor = Color.White
-  }
-  
-  def hydro() = {
-    val hydro_sphere1 = new Sphere(100) {
-      material = hydro_mat
-    }
-    val hydro_cylinder1 = new Cylinder(25, 200) {
-      material = conn_mat
-      translateX = 200
-      rotationAxis = Rotate.ZAxis
-      rotate = 90
-    }
-    new Group(hydro_sphere1, hydro_cylinder1)
-  }
-  
-  def molecule() = {
-    val water = new Sphere(200) {
-      material = water_mat
-    }
-    
-    val hydrogen1 = hydro()
-    val hydrogen2 = hydro()
-    val molecule  = new Group(water, hydrogen1, hydrogen2)
-    
-    hydrogen1.transforms = new Rotate( -45, Rotate.ZAxis) :: new Translate(-500, 0, 0) :: Nil
-    hydrogen2.transforms = new Rotate(-135, Rotate.ZAxis) :: new Translate(-500, 0, 0) :: Nil
-    molecule.transforms  = new Translate(0, -100, 500) :: Nil
-    molecule
-  }
-  
   camera = new PerspectiveCamera(true) {
     translateZ = -1000
     nearClip = 0.1
     farClip = 2000
     fieldOfView = 35
   }
-  root = molecule
+  
+  root = new Water {
+    transforms = new Translate(0, 0, 1000) :: Nil
+  }
 }
 
 class ScalaHUD(outer : Stage) extends HBox {
